f8897e1410c8c3a884a421d9f5b60bf12f5e8335
Javier Benavides <javier@neatpagos.com>
2024-09-08 23:12:07 -0300

    chore: Add app and firebase configuration files

---
+++ app.config.js
@@ -0,0 +1,19 @@
+// app.config.js
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "your-app-name",
+    slug: "your-app-slug",
+    extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
+  }
+};
---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: Constants.manifest.extra.firebaseApiKey,
+    authDomain: Constants.manifest.extra.firebaseAuthDomain,
+    projectId: Constants.manifest.extra.firebaseProjectId,
+    storageBucket: Constants.manifest.extra.firebaseStorageBucket,
+    messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
+    appId: Constants.manifest.extra.firebaseAppId,
+    databaseURL: Constants.manifest.extra.firebaseDatabaseURL,
+    measurementId: Constants.manifest.extra.firebaseMeasurementId
+  };
+
+const app = initializeApp(firebaseConfig);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase

6bc02a69b3e065ebffd6658ab2177d524d16431f
Javier Benavides <javier@neatpagos.com>
2024-09-08 23:25:36 -0300

    chore: Update firebase configuration to use environment variables

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -9,14 +9,14 @@ import { initializeApp } from 'firebase/app';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: Constants.manifest.extra.firebaseApiKey,
-    authDomain: Constants.manifest.extra.firebaseAuthDomain,
-    projectId: Constants.manifest.extra.firebaseProjectId,
-    storageBucket: Constants.manifest.extra.firebaseStorageBucket,
-    messagingSenderId: Constants.manifest.extra.firebaseMessagingSenderId,
-    appId: Constants.manifest.extra.firebaseAppId,
-    databaseURL: Constants.manifest.extra.firebaseDatabaseURL,
-    measurementId: Constants.manifest.extra.firebaseMeasurementId
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
   };
 
 const app = initializeApp(firebaseConfig);
